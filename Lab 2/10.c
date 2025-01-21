b2c5dfce6697ab4b658a6f378d32fcb811eba9c8
clickTwice26 <shagatoc@gmail.com>
2025-01-21 18:43:18 +0600

    problem 10 solved from array

---
+++ Lab 2/10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int numberOfTestCases;
+	scanf("%d", &numberOfTestCases);
+	int testCases[numberOfTestCases];
+	for (int i = 0; i < numberOfTestCases; i++){
+		scanf("%d", &testCases[i]);
+	}
+	int largestDifference = 0;
+	int temp;
+	for (int i = 0; i < numberOfTestCases; i++){
+		for(int j = i+1; j < numberOfTestCases; j++){
+			if(testCases[i] < testCases[j]){
+
+			}else{
+				continue;
+			}
+			// printf("Passed for %d %d\n", testCases[i], testCases[j]);
+			temp = (testCases[i] - testCases[j]);
+
+			if(abs(temp) > largestDifference){
+				largestDifference = abs(testCases[i] - testCases[j]);
+			}
+		}
+	}
+	printf("%d", largestDifference);
+}
