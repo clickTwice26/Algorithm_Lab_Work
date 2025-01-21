4df7be948707847ad06adddf9683914e691c7008
clickTwice26 <shagatoc@gmail.com>
2025-01-21 18:01:25 +0600

    problem 7 solved

---
+++ Lab 2/7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	int numberOfTestCase;
+	int targetSum;
+	scanf("%d %d", &numberOfTestCase, &targetSum);
+	int testCases[numberOfTestCase];
+	for (int i = 0; i < numberOfTestCase; i++){
+		scanf("%d", &testCases[i]);
+	}
+	int x;
+	int y;
+	for(int i = 0; i < numberOfTestCase; i++){
+		x = testCases[i];
+		for(int j=i+1; j <numberOfTestCase; j++){
+			y = testCases[j];
+			if((x+y) == targetSum){
+				printf("(%d, %d)\n", x, y);
+			}
+		}
+	}
+
+	return 0;
+}
