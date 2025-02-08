fd4b7022f666dd79b45e5876202e622e3dfc0935
clickTwice26 <shagatoc@gmail.com>
2025-02-08 21:20:26 +0600

    10 completed

--- Lab 2/string/10.c
+++ Lab 2/string/10.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 
-int main(){
-    char test[3] = "abc";
-    for(int i = 0; i < 3; i++){
-        for (int j = i+1; j < 3; j++){
-            printf("%c ", test[j]);
-
+int main() {
+    char str_array[100];
+    scanf("%s",str_array);
+    
+    int string_length = 0;
+    while (1) {
+        if (str_array[string_length]== '\0') {
+            break;
         }
+        string_length = string_length+ 1;
     }
 
+    int is_first_print=1;
+
+    for(int current_length = 1; current_length <= string_length; current_length++) {
+        for (int starting_pos = 0;starting_pos <= string_length - current_length; starting_pos++) {
+            if (is_first_print== 1) {
+                is_first_print = 0;
+            } else{
+                printf(",");
+            }
+            
+            for (int char_pos = starting_pos; char_pos< starting_pos + current_length; char_pos++) {
+                char current_char = str_array[char_pos];
+                printf("%c",current_char);
+            }
+        }
+    }
+    return 0;
 }
